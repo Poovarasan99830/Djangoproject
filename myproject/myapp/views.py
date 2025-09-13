36cb7379eb9fa9425ef7bda082d911301758139a
Poovarasan99830 <kingpoovarasan49@gmail.com>
2025-09-13 16:25:05 +0530

    first commit

---
+++ myproject/myapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("welcome django......!")
+
+def fan(request):
+    return render(request,"one.html")
+
+
+def Television(request):
+    return render(request,"bootstrap.html")
+
+def Television2(request):
+    return render(request,"bootstrap2.html")
